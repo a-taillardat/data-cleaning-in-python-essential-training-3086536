260f6794109dc01fb783695cdc5f87c355e7c451
Alexis Taillardat <102881202+a-taillardat@users.noreply.github.com>
2024-05-23 20:15:36 +0000

    Add notebooks chapters 3_4 to 3_5

--- Ch03/challenge/rides.py
+++ Ch03/challenge/rides.py
@@ -1,5 +1,6 @@
 # %%
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('rides.csv')
 df
@@ -8,3 +9,28 @@ df
 # - Missing values are not allowed
 # - A plate must be a combination of at least 3 upper case letters or digits
 # - Distance much be bigger than 0
+# %%
+df.dtypes
+# %%
+# Missing values
+
+# verification for string values ''
+df[['name', 'plate']] = df[['name', 'plate']]\
+  .apply(lambda x: x.str.strip()).replace('', np.nan)
+
+## or:
+#df['name'] = df['name'].str.strip()
+#df['plate'] = df['plate'].str.strip()
+#df.replace('', np.nan)
+
+df.dropna(inplace=True)
+df
+
+# %%
+# Plates
+df = df[(df['plate'].str.len() >= 3) &\
+        (df['plate'] == df['plate'].str.upper())]
+df
+# %%
+df = df[df['distance'] > 0]
+df
